11c8ad34bbb2f1eef301539bbad634c75b9d4ded
Rudi Starcevic <ooly.me@gmail.com>
2015-04-24 14:34:28 +0800

    Default routes unit tests

--- httpd/routes/routes-default.js
+++ httpd/routes/routes-default.js
@@ -14,16 +14,21 @@ module.exports = function (app) {
     // so that koa doesn't assign 200 on body=
     this.status = 404;
 
-    switch (this.accepts('html', 'json')) {
+    //switch (this.accepts('html', 'json')) {
+    switch (this.accepts('html', 'text', 'application/vnd.api+json', 'json')) {
+
       case 'html':
         this.type = 'html';
         this.body = '<p>Page Not Found</p>';
         break;
+
+      case 'application/vnd.api+json':
       case 'json':
         this.body = {
           message: 'Page Not Found'
         };
         break;
+
       default:
         this.type = 'text';
         this.body = 'Page Not Found';
--- httpd/routes/routes-default.spec.js
+++ httpd/routes/routes-default.spec.js
@@ -13,22 +13,76 @@ require('./routes-default')(app);
 
 var server = http.createServer(app.callback());
 
-describe('Default Routes', function () {
+describe.only('Default Routes', function () {
 
-  it('should 404 Page Not Found', function (done) {
-    request(server)
-      .get('/not/found')
-      .expect(404)
-      .end(function (err, res) {
-        if (err) {
-          should.not.exist(err);
-          return done(err);
-        }
-        res.statusCode.should.equal(404);
-        res.text.should.equal('<p>Page Not Found</p>');
-        done();
+  describe('GET', function () {
 
-      });
+    it('should 404 Page Not Found in HTML', function (done) {
+      request(server)
+        .get('/not/found')
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            should.not.exist(err);
+            return done(err);
+          }
+          res.text.should.equal('<p>Page Not Found</p>');
+          done();
+
+        });
+    });
+
+    it('should 404 Page Not Found in vnd.api+json', function (done) {
+      request(server)
+        .get('/not/found')
+        .set('Accept', 'application/vnd.api+json')
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            should.not.exist(err);
+            return done(err);
+          }
+          res.text.should.match(/{"message":"Page Not Found"}/);
+          done();
+
+        });
+    });
+
+    it('should 404 Page Not Found in json', function (done) {
+      request(server)
+        .get('/not/found')
+        .set('Accept', 'application/json')
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            should.not.exist(err);
+            return done(err);
+          }
+          res.text.should.match(/{"message":"Page Not Found"}/);
+          done();
+
+        });
+    });
+
+    it('should 404 default Page Not Found in text', function (done) {
+      request(server)
+        .get('/not/found')
+        .set('Accept', 'application/unknown')
+        .expect(404)
+        .end(function (err, res) {
+          if (err) {
+            should.not.exist(err);
+            return done(err);
+          }
+          res.text.should.match(/^Page\ Not\ Found$/);
+          done();
+
+        });
+    });
+
+
+
+
   });
 
 });
